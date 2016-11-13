2d11c4aa01c6aa7a1953b24688a67c296b265101
obaizali <gomez.obaiz@gmail.com>
2016-11-13 20:21:07 +0530

    js test page

--- test_settings/settings.js
+++ test_settings/settings.js
@@ -1,12 +1,34 @@
 var test = [
     {
-        test_id: 'engg',
+        test_id: 'gate',
         sub_test_id: 1,
-        duration: 160,
-        no_of_ques: 20,
-        negative: 2,
+        sub_test_name: 'CSE',
+        duration: 5400,
+        no_of_ques: 35,
+        negative: 0.33,
         positive: 1,
         no_answer: 0,
-        rules: 'dssadsadsadsa'
+        rules: 'dssadsadsadsa',
+        double_type: true,
+        d_no_of_ques: 15,
+        d_n_marks: 1.33,
+        d_p_marks: 2,
+        d_no_marks: 0
+    },
+    {
+        test_id: 'aptitude',
+        sub_test_id: 1,
+        sub_test_name:'quant',
+        duration: 5400,
+        no_of_ques: 30,
+        negative: 0,
+        positive: 1,
+        no_answer: 0,
+        rules: 'dssadsadsadsa',
+        double_type: false,
+        d_no_of_ques: null,
+        d_n_marks: null,
+        d_p_marks: null,
+        d_no_marks: null
     }
 ];
