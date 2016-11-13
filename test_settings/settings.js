753ba20035c071ec1476ef31d5a1520e2039ebea
subhamayd2 <bloodyshower@gmail.com>
2016-11-13 18:44:38 +0530

    settings added

---
+++ test_settings/settings.js
@@ -0,0 +1,12 @@
+var test = [
+    {
+        test_id: 'engg',
+        sub_test_id: 1,
+        duration: 160,
+        no_of_ques: 20,
+        negative: 2,
+        positive: 1,
+        no_answer: 0,
+        rules: 'dssadsadsadsa'
+    }
+];
