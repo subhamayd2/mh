def5aef676d884f12af4d0d5e2d55ade6b2734a8
obaizali <gomez.obaiz@gmail.com>
2016-11-22 12:46:38 +0530

    gate js

--- exam/js/settings.js
+++ exam/js/settings.js
@@ -1,7 +1,7 @@
 var test = [
     {
         test_id: 'gate',
-        sub_test_id: 2,
+        sub_test_id: 1,
         sub_test_name: 'Aerospace Engineering',
         duration: 5400,
         no_of_ques: 35,
@@ -17,7 +17,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 1,
+        sub_test_id: 2,
         sub_test_name: 'Biotechnology',
         duration: 5400,
         no_of_ques: 35,
@@ -33,7 +33,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 1,
+        sub_test_id: 3,
         sub_test_name: 'Civil Engineering',
         duration: 5400,
         no_of_ques: 35,
@@ -49,7 +49,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 1,
+        sub_test_id: 4,
         sub_test_name: 'Chemical Engineering',
         duration: 5400,
         no_of_ques: 35,
@@ -65,7 +65,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 1,
+        sub_test_id: 5,
         sub_test_name: 'Computer Science and Information Technology',
         duration: 5400,
         no_of_ques: 35,
@@ -81,7 +81,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 1,
+        sub_test_id: 6,
         sub_test_name: 'Electronics and Communication Engineering',
         duration: 5400,
         no_of_ques: 35,
@@ -97,7 +97,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 1,
+        sub_test_id: 7,
         sub_test_name: 'Electrical Engineering',
         duration: 5400,
         no_of_ques: 35,
@@ -113,7 +113,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 1,
+        sub_test_id: 8,
         sub_test_name: 'Instrumentation Engineering',
         duration: 5400,
         no_of_ques: 35,
@@ -129,7 +129,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 1,
+        sub_test_id: 9,
         sub_test_name: 'Mechanical Engineering',
         duration: 5400,
         no_of_ques: 35,
@@ -145,7 +145,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 1,
+        sub_test_id: 10,
         sub_test_name: 'Petroleum Engineering',
         duration: 5400,
         no_of_ques: 35,
@@ -161,7 +161,7 @@ var test = [
     },
     {
         test_id: 'gate',
-        sub_test_id: 2,
+        sub_test_id: 11,
         sub_test_name: 'Production and Industrial Engineering',
         duration: 5400,
         no_of_ques: 35,
