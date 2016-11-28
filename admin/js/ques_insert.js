c3c229340e6d37e653d0ab44c06679cb0b7965c3
obaizali <gomez.obaiz@gmail.com>
2016-11-28 12:53:21 +0530

    add question

---
+++ admin/js/ques_insert.js
@@ -0,0 +1,99 @@
+/**
+ * Created by Obaiz on 11/28/2016.
+ */
+var ques_insert = [
+    {
+        GATE:[
+            {
+                1:'Aerospace Engineering',
+                2:'Biotechnology',
+                3:'Civil Engineering',
+                4:'Chemical Engineering',
+                5:'Computer Science and Information Technology',
+                6:'Electronics and Communication Engineering',
+                7:'Electrical Engineering',
+                8:'Instrumentation Engineering',
+                9:'Mechanical Engineering',
+                10:'Petroleum Engineering',
+                11:'Production and Industrial Engineering'
+            }
+        ],
+        CAT:[
+            {
+                1:'General Aptitude',
+                2:'Verbal Aptitude',
+                3:'Logical Aptitude',
+                4:'Quantitative Aptitude'
+            }
+
+        ],
+        IES:[
+            {
+                1:'Civil Engineering',
+                2:'Mechanical Engineering',
+                3:'Electrical Engineering',
+                4:'Electronics and Communication Engineering'
+            }
+        ],
+        IIT:[
+            {
+                1:'Mathematics',
+                2:'Physics',
+                3:'Chemistry'
+            }
+        ],
+        AIPMT:[
+            {
+                1:'Biology',
+                2:'Physics',
+                3:'Chemistry'
+            }
+        ],
+        MBA:[
+            {
+                1: 'MBA Branch Selector Assessment'
+            }
+        ],
+        BTECH:[
+            {
+                1:'Engineering Branch Selector Assessment'
+            }
+        ],
+        NINE:[
+            {
+                1:'9th & 10th Branch Selector Assessment'
+            }
+        ],
+        EMPLOYEE:[
+            {
+                1:'Employibility Test'
+            }
+        ],
+        AMCAT:[
+            {
+                1:'AMCAT Mock'
+            }
+        ],
+        CTS:[
+            {
+                1:'CTS Mock'
+            }
+        ],
+        TCS:[
+            {
+                1:'TCS Mock'
+            }
+        ],
+        WIPRO:[
+            {
+                1:'WIPRO Mock'
+            }
+        ],
+        INFOSYS:[
+            {
+                1:'INFOSYS Mock'
+            }
+        ]
+
+    }
+];
---
+++ admin/ques.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <select>
+        <option value="gate">GATE</option>
+        <option value="cat">CAT</option>
+        <option value="nine">Stream selector for 9th and 10th</option>
+        <option value="iit">IIT-JEE</option>
+        <option value="aipmt">AIPMT</option>
+        <option value="amcat">AMCAT</option>
+        <option value="ies">IES</option>
+        <option value="btech">Stream selector for B.Tech</option>
+        <option value="mba">Stream selector for MBA</option>
+        <option value="employee">Employibility test</option>
+        <option value="cts">CTS</option>
+        <option value="tcs">TCS</option>
+        <option value="wipro">WIPRO</option>
+        <option value="infosys">INFOSYS</option>
+    </select>
+    <select>
+        
+    </select>
+</body>
+</html>
